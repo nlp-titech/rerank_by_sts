79e3bdbe43135cd7b594cd1fd1e71316326bda1b
meshidenn <mesitahiro@hotmail.com>
2021-06-14 21:40:24 +0900

    add cut rank

--- utils/statistical_significance.py
+++ utils/statistical_significance.py
@@ -35,44 +35,46 @@ import pytrec_eval
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('qrel')
-    parser.add_argument('run', nargs=2)
+    parser.add_argument("qrel")
+    parser.add_argument("run", nargs=2)
 
     # A bit too strict, as it does not allow for parametrized measures,
     # but sufficient for the example.
-    parser.add_argument('--measure',
-                        choices=pytrec_eval.supported_measures,
-                        required=True)
+    parser.add_argument("--measure", choices=pytrec_eval.supported_measures, required=True)
+
+    parser.add_argument("--rank")
 
     args = parser.parse_args()
 
     assert os.path.exists(args.qrel)
     assert all(map(os.path.exists, args.run))
 
-    with open(args.qrel, 'r') as f_qrel:
+    with open(args.qrel, "r") as f_qrel:
         qrel = pytrec_eval.parse_qrel(f_qrel)
 
-    with open(args.run[0], 'r') as f_run:
+    with open(args.run[0], "r") as f_run:
         first_run = pytrec_eval.parse_run(f_run)
 
-    with open(args.run[1], 'r') as f_run:
+    with open(args.run[1], "r") as f_run:
         second_run = pytrec_eval.parse_run(f_run)
 
-    evaluator = pytrec_eval.RelevanceEvaluator(
-        qrel, {args.measure})
+    evaluator = pytrec_eval.RelevanceEvaluator(qrel, {args.measure})
 
     first_results = evaluator.evaluate(first_run)
     second_results = evaluator.evaluate(second_run)
 
-    query_ids = list(
-        set(first_results.keys()) & set(second_results.keys()))
+    query_ids = list(set(first_results.keys()) & set(second_results.keys()))
 
-    first_scores = [
-        first_results[query_id][args.measure] for query_id in query_ids]
-    second_scores = [
-        second_results[query_id][args.measure] for query_id in query_ids]
+    if args.rank:
+        measure = f"{args.measure}_{args.rank}"
+    else:
+        measure = args.measure
+
+    first_scores = [first_results[query_id][measure] for query_id in query_ids]
+    second_scores = [second_results[query_id][measure] for query_id in query_ids]
 
     print(scipy.stats.ttest_rel(first_scores, second_scores))
+
 
 if __name__ == "__main__":
     sys.exit(main())
